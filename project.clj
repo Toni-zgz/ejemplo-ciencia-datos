6ce7b9831d581889124d32bde955d93cab8025e3
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-05-19 22:23:10 +0200

    - Incorporamos la libreria incanter - cargamos el dataset

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [incanter/incanter-core "1.9.3"]
+                 [incanter/incanter-charts "1.9.3"]]
   :repl-options {:init-ns ejemplo-ciencia-datos.core}
   :plugins [[org.clojars.benfb/lein-gorilla "0.6.0"]])
