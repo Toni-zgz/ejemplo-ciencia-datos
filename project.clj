f4dfdaaa78235eaebf5110d9f544ea1c46e08a96
Antonio Julián Arrieta Cuartero <ajaczgz@yahoo.es>
2023-05-21 16:58:45 +0200

    Resolvemos un par de errores

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [incanter "1.9.3"]
-                 [incanter-gorilla "0.1.0"]]
+                 [incanter-gorilla "0.1.0"]
+                 [net.mikera/core.matrix "0.63.0"]]
   :repl-options {:init-ns ejemplo-ciencia-datos.core}
   :plugins [[org.clojars.benfb/lein-gorilla "0.6.0"]])
